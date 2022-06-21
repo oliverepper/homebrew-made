b8234b73c1d3607633e76d19cd69f43f8757f3b0
Oliver Epper <oliver.epper@gmail.com>
2022-06-21 10:26:18 +0200

    version 0.6

--- Formula/pjproject-apple-platforms.rb
+++ Formula/pjproject-apple-platforms.rb
@@ -1,15 +1,15 @@
 class PjprojectApplePlatforms < Formula
   desc "PJSIP project on Apple platforms"
   homepage "https://github.com/oliverepper/pjproject-apple-platforms"
-  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: 0.5
+  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: 0.6
   license "MIT"
   head "https://github.com/oliverepper/pjproject-apple-platforms.git", branch: "main"
   # sha256 "6084cf12da5a97475375d281d0ecd96bb745a48b9aa61a23f7064da9262e60da"
 
   bottle do
-    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.5'
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0d62ed2306e1c4a90d33b02518c48b6b6005672baa5c15015f5a75f4f34a2b50"
-    sha256 cellar: :any_skip_relocation, monterey: "0d62ed2306e1c4a90d33b02518c48b6b6005672baa5c15015f5a75f4f34a2b50"
+    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.6'
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2484eeba66b8f0364afe1a5a8b5bf5180a1a36afd2673611f2d180228bc61626"
+    sha256 cellar: :any_skip_relocation, monterey: "2484eeba66b8f0364afe1a5a8b5bf5180a1a36afd2673611f2d180228bc61626"
   end
   
   def install
