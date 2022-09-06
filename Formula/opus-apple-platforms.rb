12e88f66aa9e2e4ce03f8b82989686a88eff5012
Oliver Epper <oliver.epper@gmail.com>
2022-09-06 11:44:48 +0200

    bottle and dependencies

--- Formula/opus-apple-platforms.rb
+++ Formula/opus-apple-platforms.rb
@@ -7,18 +7,20 @@ class OpusApplePlatforms < Formula
   
   bottle do
     root_url 'https://github.com/oliverepper/opus-apple-platforms/releases/download/0.1'
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "af2383f9457e180c68ebbc7e5136a461c55aa0aa1bb43f9a09c3620f4d01c76a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5ae3f606cf48ac5a4f1904e23be022105675d8170aa0417313da91e4eed47de6"
   end
-    
+
   depends_on "autoconf" => [:build]
   depends_on "automake" => [:build]
   depends_on "libtool" => [:build]
+  depends_on "gawk" => [:build]
+  depends_on :xcode
 
   def install
-    # ENV.deparallelize # IMPORTANT!
-    # # there might be a better way
-    # ENV["CC"] = "/usr/bin/clang"
-    # ENV["CXX"] = "/usr/bin/clang++"
+    ENV.deparallelize # IMPORTANT!
+    # there might be a better way
+    ENV["CC"] = "/usr/bin/clang"
+    ENV["CXX"] = "/usr/bin/clang++"
     system "sh", "start.sh"
     system "sh", ".install.sh", prefix.to_s
   end
