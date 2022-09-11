2ba2199c723a27263f286c6a0dfc66c07ba19078
Oliver Epper <oliver.epper@gmail.com>
2022-09-11 11:07:51 +0200

    pjproject 0.7.9

--- Formula/pjproject-apple-platforms.rb
+++ Formula/pjproject-apple-platforms.rb
@@ -1,15 +1,13 @@
 class PjprojectApplePlatforms < Formula
   desc "PJSIP project on Apple platforms"
   homepage "https://github.com/oliverepper/pjproject-apple-platforms"
-  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.6.2'
+  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.7.9'
   license "MIT"
   head "https://github.com/oliverepper/pjproject-apple-platforms.git", branch: "main"
-  # sha256 "6084cf12da5a97475375d281d0ecd96bb745a48b9aa61a23f7064da9262e60da"
 
   bottle do
-    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.6.2'
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e2da3ab06902c82726b901e462a1ac6fd3046767a1d1cba774e79cba2e6add96"
-    sha256 cellar: :any_skip_relocation, monterey: "e2da3ab06902c82726b901e462a1ac6fd3046767a1d1cba774e79cba2e6add96"
+    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.7.9'
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b8561967462e64b70f07e26c812c61e5036a969975a4968b9fd063f5dde8c094"
   end
   
   def install
@@ -17,8 +15,7 @@ class PjprojectApplePlatforms < Formula
     # there might be a better way
     ENV["CC"] = "/usr/bin/clang"
     ENV["CXX"] = "/usr/bin/clang++"
-    system "sh", "start.sh"
-    system "sh", "install.sh", prefix.to_s
+    system "sh", "start.sh", prefix.to_s
   end
 
   test do
