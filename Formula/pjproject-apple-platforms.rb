e74de46ce7267982132b23a5aa818b6e8fa37ab1
Oliver Epper <oliver.epper@gmail.com>
2022-09-23 10:43:46 +0200

    pjproject-apple-platforms 0.8.1

--- Formula/pjproject-apple-platforms.rb
+++ Formula/pjproject-apple-platforms.rb
@@ -1,13 +1,13 @@
 class PjprojectApplePlatforms < Formula
   desc "PJSIP project on Apple platforms"
   homepage "https://github.com/oliverepper/pjproject-apple-platforms"
-  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.8'
+  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.8.1'
   license "MIT"
   head "https://github.com/oliverepper/pjproject-apple-platforms.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.8'
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "be33f288ae542404873120204e7f44008a1c8b627cec7355e9d7e58bf28fcb41"
+    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.8.1'
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "011fff6b94fa3b5657ab068943c833c55b5290199613c94f0a6d2041050fd814"
   end
   
   def install
