6d69dfa69298d8cc8bdd1b29297c5169a86b7310
Oliver Epper <oliver.epper@gmail.com>
2022-10-11 11:07:39 +0200

    pjproject-apple-platforms 0.9

--- Formula/pjproject-apple-platforms.rb
+++ Formula/pjproject-apple-platforms.rb
@@ -1,14 +1,16 @@
 class PjprojectApplePlatforms < Formula
   desc "PJSIP project on Apple platforms"
   homepage "https://github.com/oliverepper/pjproject-apple-platforms"
-  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.8.1'
+  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.9'
   license "MIT"
   head "https://github.com/oliverepper/pjproject-apple-platforms.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.8.1'
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "011fff6b94fa3b5657ab068943c833c55b5290199613c94f0a6d2041050fd814"
+    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.9'
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0c92b7d29f03ea6a0b3dcdb3c056a239ea9005a7ce4d42b10ff6a01477f97c74"
   end
+
+  depends_on :xcode
   
   def install
     ENV.deparallelize # IMPORTANT!
