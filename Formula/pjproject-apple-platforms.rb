2efa07686b0ed7c309811b0e2382708e4bc2755f
Oliver Epper <oliver.epper@gmail.com>
2023-02-16 14:57:45 +0100

    0.10.5

--- Formula/pjproject-apple-platforms.rb
+++ Formula/pjproject-apple-platforms.rb
@@ -1,16 +1,17 @@
 class PjprojectApplePlatforms < Formula
   desc "PJSIP project on Apple platforms"
   homepage "https://github.com/oliverepper/pjproject-apple-platforms"
-  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.10.4'
+  url "https://github.com/oliverepper/pjproject-apple-platforms.git", tag: '0.10.5'
   license "MIT"
   head "https://github.com/oliverepper/pjproject-apple-platforms.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.10.4'
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "170211116e3b6967b3dd5006f7e41b144fb9f80271ef89901bcc0fa26d536310"
+    root_url 'https://github.com/oliverepper/pjproject-apple-platforms/releases/download/0.10.5'
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "9b640aeb5f034c20ef3098f86022617230e110511268893e37f386f3e44ca126"
   end
 
   depends_on "cmake" => [:build]
+  depends_on "llvm" => [:build]
   depends_on :xcode
   
   def install
